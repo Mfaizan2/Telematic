32c45a25ec489030ea3a4046cf69f0f48f757e6a
Wajahat Khan <wajahatkhan@gmail.com>
2025-03-03 16:20:39 +0500

    for loop commit

--- mysite/views.py
+++ mysite/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 def home (request):
     data =  {
         "title": "Home",
+        "clist": ['Python', "django", "laravel"],
+        "students":[
+            {"name":"Ali", "id": 24},
+            {"name":"Arman", "id": 25},
+        ]
     }
     return render(request, "index.html", data)
 
