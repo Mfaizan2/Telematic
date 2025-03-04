d46947284986693bed2607158b6450e99dddebbb
Wajahat Khan <wajahatkhan@gmail.com>
2025-03-04 16:59:54 +0500

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo',  # Corrected from 'Nmae' to 'NAME'
+        'USER': 'postgres',  # 'User' → 'USER'
+        'PASSWORD': 'wajahat 123',  # 'Password' → 'PASSWORD'
+        'HOST': 'localhost',  # 'Host' → 'HOST'
+        'PORT': '5432',  # 'Port' → 'PORT'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
